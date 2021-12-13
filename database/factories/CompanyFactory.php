d0ee12a9d55d292e9b4f6d3bd9e0da4f5486356d
teitsuki <teitsuki@icloud.com>
2021-12-13 22:05:00 +0900

    テストデータのシーダー作成

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -25,7 +25,7 @@ class CompanyFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'name' => $this->faker->company(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
--- database/seeders/JobOfferSeeder.php
+++ database/seeders/JobOfferSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Company;
+use App\Models\JobOffer;
+use App\Models\Occupation;
+use Carbon\Carbon;
 
 class JobOfferSeeder extends Seeder
 {
@@ -13,6 +17,42 @@ class JobOfferSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $companies = Company::take(10)->get();
+        foreach ($companies as $company) {
+            // 期限切れ且つ募集終了(期限昨日)
+            JobOffer::create([
+                'company_id' => $company->id,
+                'occupation_id' => Occupation::inRandomOrder()->first()->id,
+                'title' => $company->name . 'の求人情報1のタイトル',
+                'description' => $company->name . 'の求人情報1の本文',
+                'due_date' => Carbon::yesterday()->toDateString(),
+                'status' => 0,
+            ]);
+            // 募集終了(期限翌月)
+            JobOffer::create([
+                'company_id' => $company->id,
+                'occupation_id' => Occupation::inRandomOrder()->first()->id,
+                'title' => $company->name . 'の求人情報2のタイトル',
+                'description' => $company->name . 'の求人情報2の本文',
+                'due_date' => Carbon::now()->firstOfMonth()->addMonth(1)->toDateString(),
+                'status' => 0,
+            ]);
+            // 募集中(期限翌月)
+            JobOffer::create([
+                'company_id' => $company->id,
+                'occupation_id' => Occupation::inRandomOrder()->first()->id,
+                'title' => $company->name . 'の求人情報3のタイトル',
+                'description' => $company->name . 'の求人情報3の本文',
+                'due_date' => Carbon::now()->firstOfMonth()->addMonth(1)->toDateString(),
+            ]);
+            // 募集中(期限翌々月)
+            JobOffer::create([
+                'company_id' => $company->id,
+                'occupation_id' => Occupation::inRandomOrder()->first()->id,
+                'title' => $company->name . 'の求人情報4のタイトル',
+                'description' => $company->name . 'の求人情報4の本文',
+                'due_date' => Carbon::now()->firstOfMonth()->addMonth(2)->toDateString(),
+            ]);
+        }
     }
 }
